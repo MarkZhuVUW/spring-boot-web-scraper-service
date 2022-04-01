869f8505362d86dad9b36f9f391b9851f3dd58b2
MarkZhuVUW <zdy120939259@outlook.com>
2022-04-01 19:01:55 +1300

    no message

--- scripts/deploy-github-releases.sh
+++ scripts/deploy-github-releases.sh
@@ -8,6 +8,6 @@ export VERSION=${VERSION:=0.0.1}
 echo $VERSION;
 go get github.com/tcnksm/ghr
 mkdir artifacts
-cp api.jar ./artifacts
-cp client.jar ./artifacts
+cp ../api.jar ./artifacts
+cp ../client.jar ./artifacts
 ghr -t "${GITHUB_TOKEN}" -u "${CIRCLE_PROJECT_USERNAME}" -r "${CIRCLE_PROJECT_REPONAME}" -c "${CIRCLE_SHA1}" -delete ${VERSION} ./artifacts/
