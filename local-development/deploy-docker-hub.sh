2ee9813098cde7d9c623d0a392399fb4ee82c394
MarkZhuVUW <zdy120939259@outlook.com>
2022-07-29 07:30:44 +1200

    no message

--- local-development/deploy-docker-hub.sh
+++ local-development/deploy-docker-hub.sh
@@ -9,11 +9,9 @@ echo $ENVIRONMENT
 echo $AWS_REGION
 
 export TERM=${TERM:=xterm}
-export DOCKERHUB_USERNAME=${DOCKERHUB_USERNAME:=zdy120939259}
-export DOCKERHUB_PASSWORD=${DOCKERHUB_PASSWORD:=Mark19960630}
 export GITHUB_USERNAME=${GITHUB_USERNAME:=MarkZhuVUW}
 
-echo $DOCKERHUB_PASSWORD | docker login -u $DOCKERHUB_USERNAME --password-stdin
+echo "$DOCKERHUB_PASSWORD" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
 
 
 docker-compose build
